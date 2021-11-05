a744985a545ce679550ca21a48ef00b7386202ae
ashpex <ashpex@pm.me>
2021-11-05 20:58:23 +0700

    remove unused permission

--- app/src/main/java/com/example/testgallery/MainActivity.java
+++ app/src/main/java/com/example/testgallery/MainActivity.java
@@ -104,7 +104,7 @@ public class MainActivity extends AppCompatActivity {
         TedPermission.create()
                 .setPermissionListener(permissionlistener)
                 .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
-                .setPermissions(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA)
+                .setPermissions(Manifest.permission.READ_EXTERNAL_STORAGE)
                 .check();
     }
 }
