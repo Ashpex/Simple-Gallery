acaee2c6d8d300d876417da039aed0f67f3e934e
ashpex <ashpex@pm.me>
2021-11-08 09:25:11 +0700

    Add function to read image's info (exif data)

--- app/src/main/java/com/example/testgallery/PictureActivity.java
+++ app/src/main/java/com/example/testgallery/PictureActivity.java
@@ -1,17 +1,27 @@
 package com.example.testgallery;
 
 import android.content.Intent;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.os.ParcelFileDescriptor;
+import android.widget.Toast;
+
+import java.io.FileDescriptor;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import com.bumptech.glide.Glide;
+
 
 public class PictureActivity extends AppCompatActivity {
     ImageView imageView;
     ImageView imgViewBack;
+    ImageView imgViewInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +30,21 @@ public class PictureActivity extends AppCompatActivity {
 
         imageView = findViewById(R.id.imgPicture);
         imgViewBack = findViewById(R.id.imgViewBack);
+        imgViewInfo = findViewById(R.id.imgViewInfo);
         Intent intent = getIntent();
         String thumb = intent.getStringExtra("imgSrc");
         Glide.with(this).load(thumb).into(imageView);
+
+
+        imgViewInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri targetUri = Uri.parse("file://" + thumb);
+                if(targetUri != null){
+                    showExif(targetUri);
+                }
+            }
+        });
 
 //        imageView.setImageResource(src);
 
@@ -33,4 +55,75 @@ public class PictureActivity extends AppCompatActivity {
             }
         });
     }
+
+    void showExif(Uri photoUri){
+        if(photoUri != null){
+
+            ParcelFileDescriptor parcelFileDescriptor = null;
+
+            try {
+                parcelFileDescriptor = getContentResolver().openFileDescriptor(photoUri, "r");
+                FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+
+                ExifInterface exifInterface = new ExifInterface(fileDescriptor);
+                String exif="Exif: " + fileDescriptor.toString();
+                exif += "\nChiều dài: " +
+                        exifInterface.getAttribute(ExifInterface.TAG_IMAGE_LENGTH);
+                exif += "\nChiều rộng: " +
+                        exifInterface.getAttribute(ExifInterface.TAG_IMAGE_WIDTH);
+                exif += "\n Thời gian: " +
+                        exifInterface.getAttribute(ExifInterface.TAG_DATETIME);
+                exif += "\n Nhà sản xuất: " +
+                        exifInterface.getAttribute(ExifInterface.TAG_MAKE);
+                exif += "\n Thiết bị: " +
+                        exifInterface.getAttribute(ExifInterface.TAG_MODEL);
+                exif += "\n Tác giả: " +
+                        exifInterface.getAttribute(ExifInterface.TAG_ARTIST);
+                exif += "\n Orientation: " +
+                        exifInterface.getAttribute(ExifInterface.TAG_ORIENTATION);
+                exif += "\n Cân bằng trắng " +
+                        exifInterface.getAttribute(ExifInterface.TAG_WHITE_BALANCE);
+                exif += "\n Khẩu độ: " +
+                        exifInterface.getAttribute(ExifInterface.TAG_FOCAL_LENGTH);
+                exif += "\n Flash: " +
+                        exifInterface.getAttribute(ExifInterface.TAG_FLASH);
+                exif += "\nThông tin GPS:";
+                exif += "\n TAG_GPS_DATESTAMP: " +
+                        exifInterface.getAttribute(ExifInterface.TAG_GPS_DATESTAMP);
+                exif += "\n TAG_GPS_TIMESTAMP: " +
+                        exifInterface.getAttribute(ExifInterface.TAG_GPS_TIMESTAMP);
+                exif += "\n Vĩ độ: " +
+                        exifInterface.getAttribute(ExifInterface.TAG_GPS_LATITUDE);
+                exif += "\n Vĩ độ_REF: " +
+                        exifInterface.getAttribute(ExifInterface.TAG_GPS_LATITUDE_REF);
+                exif += "\n Cao độ: " +
+                        exifInterface.getAttribute(ExifInterface.TAG_GPS_LONGITUDE);
+                exif += "\n Cao độ_REF: " +
+                        exifInterface.getAttribute(ExifInterface.TAG_GPS_LONGITUDE_REF);
+                parcelFileDescriptor.close();
+
+                Toast.makeText(getApplicationContext(),
+                        exif,
+                        Toast.LENGTH_LONG).show();
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                Toast.makeText(getApplicationContext(),
+                        "Something wrong:\n" + e.toString(),
+                        Toast.LENGTH_LONG).show();
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(getApplicationContext(),
+                        "Something wrong:\n" + e.toString(),
+                        Toast.LENGTH_LONG).show();
+            }
+
+            //String thumb = photoUri.getPath();
+
+        }else{
+            Toast.makeText(getApplicationContext(),
+                    "photoUri == null",
+                    Toast.LENGTH_LONG).show();
+        }
+    };
 }
