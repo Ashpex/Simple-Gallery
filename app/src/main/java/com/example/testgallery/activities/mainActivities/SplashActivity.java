9c983814179711e9730d55e8aa26c61aacf47eb4
ashpex <ashpex@pm.me>
2021-11-21 20:07:15 +0700

    splash screen background change according to dark/light theme, fix status bar color

--- app/src/main/java/com/example/testgallery/activities/mainActivities/SplashActivity.java
+++ app/src/main/java/com/example/testgallery/activities/mainActivities/SplashActivity.java
@@ -13,13 +13,16 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_splash);
+
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(intent);
+
                 finish();
             }
         }, 1000);
